ef1d24a8533d28e4b9d2a3f51b4ec2bc782b6c82
oktntko <oktntko@gmail.com>
2023-10-26 00:20:16 +0900

    menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,8 +7,13 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+use tauri::{CustomMenuItem, Menu, MenuItem,Submenu};
 fn main() {
+    let context = tauri::generate_context!();
+    let menu = Menu::os_default(&context.package_info().name);
+
     tauri::Builder::default()
+        .menu(menu)
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
