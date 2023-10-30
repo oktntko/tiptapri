82764fb701ddeb18bf80d4be0d6d0fd51dd9a985
oktntko <oktntko@gmail.com>
2023-10-30 16:19:28 +0900

    save & read

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,41 @@
 <script setup lang="ts">
+import { save, open } from '@tauri-apps/api/dialog';
+import { writeTextFile, readTextFile } from '@tauri-apps/api/fs';
+import * as log from 'tauri-plugin-log-api';
+
 const example = ref('');
 </script>
 
 <template>
-  <Editor v-model="example" editable />
+  <div>
+    <button
+      @click="
+        async () => {
+          const path = await open({ multiple: false });
+          if (typeof path === 'string') {
+            log.info(path);
+            example = await readTextFile(path);
+          }
+        }
+      "
+    >
+      read
+    </button>
+    <button
+      @click="
+        async () => {
+          const path = await save({});
+          if (typeof path === 'string') {
+            log.info(path);
+            await writeTextFile(path, example);
+          }
+        }
+      "
+    >
+      save
+    </button>
+    <Editor v-model="example" editable />
+  </div>
 </template>
 
 <style scoped></style>
