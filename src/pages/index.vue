67f2667d91b7a4a11ab36772963be58b9ddaa83e
oktntko <oktntko@gmail.com>
2023-10-31 16:16:05 +0900

    new file

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
-import { open } from '@tauri-apps/api/dialog';
+import { open, ask, save } from '@tauri-apps/api/dialog';
 import { exists, readTextFile, writeTextFile } from '@tauri-apps/api/fs';
 import { basename, dirname } from '@tauri-apps/api/path';
+
+// TODO: tauri の dialog が YES/NO しか選べない
+// 保存なら ①保存する②保存しない③(操作を)やめる の３つほしい
 
 type TiptapFile = {
   fullpath: string;
@@ -9,6 +12,7 @@ type TiptapFile = {
   basename: string;
   contents: string;
   originalContents: string;
+  isNew: boolean;
 };
 
 const fileList = ref<TiptapFile[]>([]);
@@ -18,10 +22,30 @@ window.addEventListener('keydown', async (e) => {
   if (current.value && e.ctrlKey && e.code == 'KeyS') {
     e.preventDefault();
     await saveFile(current.value);
+    return;
+  }
+
+  if (e.ctrlKey && e.code == 'KeyN') {
+    e.preventDefault();
+    addFile();
+    return;
   }
 });
 
 async function saveFile(file: TiptapFile) {
+  if (file.isNew) {
+    const fullpath = await save();
+    if (fullpath) {
+      const [_dirname, _basename] = await Promise.all([dirname(fullpath), basename(fullpath)]);
+      file.fullpath = fullpath;
+      file.dirname = _dirname;
+      file.basename = _basename;
+      file.isNew = false;
+    } else {
+      return;
+    }
+  }
+
   await writeTextFile(file.fullpath, file.contents);
   file.originalContents = file.contents;
 }
@@ -45,6 +69,7 @@ async function openFile() {
       basename: _basename,
       contents: _contents,
       originalContents: _contents,
+      isNew: false,
     };
 
     fileList.value.push(file);
@@ -52,9 +77,23 @@ async function openFile() {
   }
 }
 
+function addFile() {
+  const file: TiptapFile = {
+    fullpath: crypto.randomUUID(),
+    dirname: '',
+    basename: 'Untitled',
+    contents: '',
+    originalContents: '',
+    isNew: true,
+  };
+
+  fileList.value.push(file);
+  current.value = file;
+}
+
 async function closeFile(file: TiptapFile) {
-  if (file.contents !== file.originalContents) {
-    if (window.confirm('save file?')) {
+  if (file.isNew || file.contents !== file.originalContents) {
+    if (await ask('save file?')) {
       await saveFile(file);
     }
   }
@@ -74,21 +113,47 @@ async function closeFile(file: TiptapFile) {
       :class="['w-20 items-center justify-between']"
     >
       <div class="flex flex-col items-center gap-6 p-4">
-        <Icon icon="twemoji:file-folder" width="48"></Icon>
-        <Icon icon="twemoji:magnifying-glass-tilted-right" width="48"></Icon>
+        <div title="explorer">
+          <Icon icon="twemoji:file-folder" width="48"></Icon>
+        </div>
+        <div title="search">
+          <Icon icon="twemoji:magnifying-glass-tilted-right" width="48"></Icon>
+        </div>
       </div>
       <div class="flex flex-col items-center gap-6 p-4">
-        <Icon icon="twemoji:gear" width="48"></Icon>
+        <div title="setting">
+          <Icon icon="twemoji:gear" width="48"></Icon>
+        </div>
       </div>
     </aside>
 
     <!-- サイドメニュー フォルダ -->
-    <aside
-      class="flex h-screen shrink-0 flex-col overflow-hidden bg-gray-50 align-middle"
-      :class="['w-80']"
-    >
-      <hr />
-      <button @click="openFile">ファイルを開く</button>
+    <aside class="flex h-screen shrink-0 flex-col overflow-hidden bg-gray-50" :class="['w-80']">
+      <header class="flex items-center justify-between p-2 uppercase">
+        explorer
+
+        <div role="menu" class="flex justify-center gap-2">
+          <button
+            type="button"
+            title="open file"
+            class="rounded p-1 hover:bg-gray-200"
+            @click="openFile"
+          >
+            <Icon icon="codicon:go-to-file" class="h-6 w-6"></Icon>
+          </button>
+          <button
+            type="button"
+            title="new file"
+            class="rounded p-1 hover:bg-gray-200"
+            @click="addFile"
+          >
+            <Icon icon="codicon:new-file" class="h-6 w-6"></Icon>
+          </button>
+          <button type="button" title="read directory" class="rounded p-1 hover:bg-gray-200">
+            <Icon icon="codicon:folder-opened" class="h-6 w-6"></Icon>
+          </button>
+        </div>
+      </header>
       <hr />
       <div>開いているフォルダ</div>
       <hr />
@@ -104,20 +169,25 @@ async function closeFile(file: TiptapFile) {
             v-for="file of fileList"
             :key="file.fullpath"
             class="cursor-pointer bg-gray-200"
-            :class="{
-              'bg-gray-50': file === current,
-            }"
+            :class="{ 'bg-gray-50': file === current }"
           >
             <a
               @click.stop="current = file"
               class="flex items-center justify-center gap-0.5 p-2"
-              :title="file.fullpath"
+              :title="file.isNew ? 'new file' : file.fullpath"
             >
               <span> {{ file.basename }}</span>
-              <div class="rounded px-1.5 py-1" v-show="file.contents !== file.originalContents">
-                <Icon icon="twemoji:yellow-circle"></Icon>
+              <div
+                class="rounded px-1.5 py-1"
+                v-show="file.isNew || file.contents !== file.originalContents"
+              >
+                <Icon :icon="file.isNew ? 'twemoji:blue-circle' : 'twemoji:yellow-circle'"></Icon>
               </div>
-              <button @click.stop="closeFile(file)" class="rounded p-1 hover:bg-gray-200">
+              <button
+                type="button"
+                class="rounded p-1 hover:bg-gray-200"
+                @click.stop="closeFile(file)"
+              >
                 <Icon icon="twemoji:multiply"></Icon>
               </button>
             </a>
